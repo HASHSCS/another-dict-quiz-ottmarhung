6b77e4290d586c2de166faca7cc695121e8f2e36
Ottmar <ottmarhung@gmail.com>
2023-10-18 10:49:41 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAA

--- solutions/dicts.py
+++ solutions/dicts.py
@@ -2,10 +2,11 @@ def get_length(my_dict):
     """
     TODO: Implement a function that returns the number of items (key-value pairs) in `my_dict`.
     """
-    pass
+    return(len(my_dict))
 
 def update_value(my_dict, key, value):
     """
     TODO: Implement a function that updates the value of `key` in `my_dict` to `value` and returns the updated dictionary.
     """
-    pass
+    my_dict[key] = value
+    return my_dict
